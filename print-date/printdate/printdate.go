a1296eb14f39a4b7158534a7b20b9d902196c9c1
Luis Rovirosa Mairlot <luis@codium.team>
2022-05-26 12:09:08 +0200

    [print-date] split into different files

--- print-date/printdate/printdate.go
+++ print-date/printdate/printdate.go
@@ -1,10 +1,5 @@
 package printdate
 
-import (
-	"fmt"
-	"time"
-)
-
 type PrintDate interface {
 	PrintCurrentDate()
 }
@@ -21,34 +16,4 @@ type printDate struct {
 func (printDate printDate) PrintCurrentDate() {
 	today := printDate.calendar.today()
 	printDate.printer.printLine(today.String())
-}
-
-type Calendar interface {
-	today() time.Time
-}
-
-func NewCalendar() Calendar {
-	return calendar{}
-}
-
-type calendar struct {
-}
-
-func (calendar calendar) today() time.Time {
-	return time.Now()
-}
-
-type Printer interface {
-	printLine(line string)
-}
-
-func NewPrinter() Printer {
-	return printer{}
-}
-
-type printer struct {
-}
-
-func (printer printer) printLine(line string) {
-	fmt.Println(line)
 }
